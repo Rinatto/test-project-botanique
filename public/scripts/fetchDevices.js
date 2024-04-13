b38c2c1a922eadfdfa956f6608eba61ae466117a
Apple <apple@MacBook-Pro-Apple.local>
2024-04-13 19:00:33 +0300

    rename path fetch

--- public/scripts/fetchDevices.js
+++ public/scripts/fetchDevices.js
@@ -1,6 +1,6 @@
 export async function fetchDevices() {
     try {
-      const apiUrl = 'http://80.78.243.62/data/devices.json';
+      const apiUrl = 'http://testinformation.ru/';
       const response = await fetch(apiUrl);
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
